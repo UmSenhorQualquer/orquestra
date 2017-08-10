e38cdc51a9de776e58862f9f3d8520f84477ffd4
Ricardo Ribeiro <ricardojvr@gmail.com>
2017-08-10 10:51:08 +0000

    .

--- orquestra/accountadapter.py
+++ orquestra/accountadapter.py
@@ -5,8 +5,6 @@ from django.conf import settings
 class OrquestraAccountAdapter(DefaultAccountAdapter):
 
 	def user_signed_up(self, request, user):
-		print(request, user)
-		print('--------------------------')
 		if hasattr( settings, 'ORQUESTRA_ALLOW_DOMAINS_LOGIN') and settings.ORQUESTRA_ALLOW_DOMAINS_LOGIN:
 			if user.email.split('@')[1] not in settings.ORQUESTRA_ALLOW_DOMAINS_LOGIN:
 				raise ImmediateHttpResponse(render_to_response('error.html'))
