b405893cc9043fa97b0e1a06684cdfd2b3f6a7ec
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-05-12 23:37:34 +0100

    fix imports

---
+++ orquestra/apps_manager.py
@@ -0,0 +1,74 @@
+import traceback, inspect
+from django.apps import apps
+from pyforms_web.basewidget import BaseWidget
+
+class AppsManager(object):
+
+	def __init__(self):
+		self._plugins_list = []
+		self.search_4_plugins()
+
+	def append(self, plugin): self._plugins_list.append(plugin)
+
+	@property
+	def plugins(self): return self._plugins_list
+
+	
+	def menu(self, user=None, menus=None):
+		res = []
+
+		for plugin_class in self.plugins:
+			if not hasattr(plugin_class, 'ORQUESTRA_MENU'): continue
+			if 	menus and \
+				(
+					not hasattr(plugin_class, 'ORQUESTRA_MENU') or \
+					not plugin_class.ORQUESTRA_MENU in menus
+				): continue
+
+			add = False
+			if hasattr(plugin_class, 'AUTHORIZED_GROUPS'):
+				if 'superuser' in plugin_class.AUTHORIZED_GROUPS and user.is_superuser:   add = True
+				if user.groups.filter(name__in=plugin_class.AUTHORIZED_GROUPS).exists():  add = True
+			else:
+				add = True
+
+			if add: res.append(plugin_class)
+
+			plugin_class.fullname = '{0}.{1}'.format( plugin_class.__module__,plugin_class.__name__)
+
+		return res
+
+
+
+	def export_settings(self, filename):
+		out = open(filename, 'w')
+
+		apps = {}
+		
+		for plugin_class in self.plugins:
+			if issubclass(plugin_class, BaseWidget):
+				print(plugin_class)
+				apps[plugin_class.__name__.lower()] = '{0}.{1}'.format(
+					plugin_class.__module__,
+					plugin_class.__name__
+				)
+
+		out.write( "PYFORMS_APPS = "+str(apps) )
+		out.close()
+
+
+	def search_4_plugins(self):
+		for app in apps.get_app_configs():
+			try:
+				apss_modulename = '{0}.apps'.format(app.module.__name__)
+
+				apps_module = __import__( apss_modulename, fromlist=[''] )
+				
+				for name in dir(apps_module):
+					obj = getattr(apps_module, name)
+					if inspect.isclass(obj) and hasattr(obj, 'LAYOUT_POSITION'):
+						self.append( obj )
+
+			except:
+				#print(app)
+				traceback.print_exc()
--- orquestra/views.py
+++ orquestra/views.py
@@ -1,85 +1,81 @@
-import os, simplejson
-from django.http 									import HttpResponse
-from django.shortcuts 								import render_to_response
-from django.contrib.auth.decorators 				import login_required
-from orquestra.management.commands.install_plugins 	import PluginsManager
-from orquestra.plugins 			 					import MenusPositions
-from pyforms_web.web 								import ApplicationsLoader
+from django.shortcuts               import render_to_response
+from django.contrib.auth.decorators import login_required
+from orquestra.apps_manager         import AppsManager
 
 @login_required
 def index(request, app_uid=None):
 
-	manager = PluginsManager()
-	
-	##### find the style and javscripts files #################################################
-	style_files, javascript_files = [], []
-	for plugin in manager.plugins:
-		for staticfile in (plugin.static_files if hasattr(plugin, 'static_files') else []):
-			if staticfile.endswith('.css'): style_files.append(staticfile)
-			if staticfile.endswith('.js'):  javascript_files.append(staticfile)
-	###########################################################################################
+    manager = AppsManager()
+    
+    ##### find the style and javscripts files #################################################
+    style_files, javascript_files = [], []
+    for plugin in manager.plugins:
+        for staticfile in (plugin.static_files if hasattr(plugin, 'static_files') else []):
+            if staticfile.endswith('.css'): style_files.append(staticfile)
+            if staticfile.endswith('.js'):  javascript_files.append(staticfile)
+    ###########################################################################################
 
-	#### load menus ###########################################################################
-	plugins4menus = sorted(manager.menu(request.user), key=lambda x: (x.ORQUESTRA_MENU,len(x.ORQUESTRA_MENU)) )
-	menus 		  = {}
-	active_menus  = {}
+    #### load menus ###########################################################################
+    plugins4menus = sorted(manager.menu(request.user), key=lambda x: (x.ORQUESTRA_MENU,len(x.ORQUESTRA_MENU)) )
+    menus         = {}
+    active_menus  = {}
 
-	running_menu = None
+    running_menu = None
 
-	for plugin_class in plugins4menus:
-		menus_options = plugin_class.ORQUESTRA_MENU.split('>')
+    for plugin_class in plugins4menus:
+        menus_options = plugin_class.ORQUESTRA_MENU.split('>')
 
-		# used to check if a menu should be activated or not
-		active_menus[menus_options[0]] = True
+        # used to check if a menu should be activated or not
+        active_menus[menus_options[0]] = True
 
-		# if an application is not running ignore the submenus
-		if app_uid is None and len(menus_options)>1: continue
-		
-		menu 			= type('MenuOption', (object,), {})
-		menu.menu_place	= menus_options[0]
-		menu.uid 		= plugin_class.UID if hasattr(plugin_class,'UID') else ''
-		menu.label 		= plugin_class.TITLE if plugin_class.TITLE else plugin_class.__name__.lower()
-		menu.order 		= plugin_class.ORQUESTRA_MENU_ORDER if hasattr(plugin_class,'ORQUESTRA_MENU_ORDER') else None
-		menu.icon  		= plugin_class.ORQUESTRA_MENU_ICON if hasattr(plugin_class, 'ORQUESTRA_MENU_ICON') else None
-		menu.anchor 	= plugin_class.__name__.lower()
-		menu.fullname 	= plugin_class.fullname # full name of the class
-		menu.parent_menu= None
-		menu.active 	= False
-		menu.submenus 	= []
-		menu.show_submenu = False
-		
-		# append main menu
-		if len(menus_options)==1:
-			menus[plugin_class.__name__] = menu
-		
-		elif len(menus_options)==2:
-			menu.parent_menu = menus[menus_options[1]]
-			menus[menus_options[1]].submenus.append( menu )
-			#menu.parent_menu.active = True
+        # if an application is not running ignore the submenus
+        if app_uid is None and len(menus_options)>1: continue
+        
+        menu            = type('MenuOption', (object,), {})
+        menu.menu_place = menus_options[0]
+        menu.uid        = plugin_class.UID if hasattr(plugin_class,'UID') else ''
+        menu.label      = plugin_class.TITLE if plugin_class.TITLE else plugin_class.__name__.lower()
+        menu.order      = plugin_class.ORQUESTRA_MENU_ORDER if hasattr(plugin_class,'ORQUESTRA_MENU_ORDER') else None
+        menu.icon       = plugin_class.ORQUESTRA_MENU_ICON if hasattr(plugin_class, 'ORQUESTRA_MENU_ICON') else None
+        menu.anchor     = plugin_class.__name__.lower()
+        menu.fullname   = plugin_class.fullname # full name of the class
+        menu.parent_menu= None
+        menu.active     = False
+        menu.submenus   = []
+        menu.show_submenu = False
+        
+        # append main menu
+        if len(menus_options)==1:
+            menus[plugin_class.__name__] = menu
+        
+        elif len(menus_options)==2:
+            menu.parent_menu = menus[menus_options[1]]
+            menus[menus_options[1]].submenus.append( menu )
+            #menu.parent_menu.active = True
 
-		if hasattr(plugin_class, 'UID') and app_uid==plugin_class.UID: 
-			running_menu = menu			
-			menu.active  = True
-			if menu.parent_menu: 
-				menu.parent_menu.show_submenu = True
-			else:
-				menu.show_submenu = True
+        if hasattr(plugin_class, 'UID') and app_uid==plugin_class.UID: 
+            running_menu = menu         
+            menu.active  = True
+            if menu.parent_menu: 
+                menu.parent_menu.show_submenu = True
+            else:
+                menu.show_submenu = True
 
-	## sort menus and submenus ######################################################################
-	menus = sorted(menus.values(), key=lambda x: x.order)
-	for menu in menus: menu.submenus = sorted(menu.submenus, key=lambda x: x.order)
-	#################################################################################################
+    ## sort menus and submenus ######################################################################
+    menus = sorted(menus.values(), key=lambda x: x.order)
+    for menu in menus: menu.submenus = sorted(menu.submenus, key=lambda x: x.order)
+    #################################################################################################
 
-	if running_menu is None and len(menus)>0: running_menu = menus[0]
+    if running_menu is None and len(menus)>0: running_menu = menus[0]
 
-	context = {'user': request.user}
-	context.update({
-		'menu_plugins': menus,
-		'active_menus': list(set(active_menus)),
-		'styles_files': style_files,
-		'javascript_files': javascript_files,
-		'running_menu': running_menu
-	})
+    context = {'user': request.user}
+    context.update({
+        'menu_plugins': menus,
+        'active_menus': list(set(active_menus)),
+        'styles_files': style_files,
+        'javascript_files': javascript_files,
+        'running_menu': running_menu
+    })
 
-	return render_to_response('authenticated_base.html', context )
+    return render_to_response('authenticated_base.html', context )
 
