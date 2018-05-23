f1707699cb0f3612360352db2bc77d1d28377096
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-05-23 13:26:18 +0100

    update imports

--- orquestra/views.py
+++ orquestra/views.py
@@ -1,4 +1,4 @@
-from pyforms                        import conf
+from confapp                        import conf
 from django.conf                    import settings
 from django.http                    import HttpResponseRedirect
 from django.shortcuts               import render_to_response
