b84ff500c331abfcfe0017430e12cb1677499d91
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-07-17 15:35:14 +0100

    Make it compatible with python 3.5

--- orquestra/apps_manager.py
+++ orquestra/apps_manager.py
@@ -69,11 +69,12 @@ class AppsManager(object):
 					obj = getattr(apps_module, name)
 					if inspect.isclass(obj) and hasattr(obj, 'LAYOUT_POSITION'):
 						self.append( obj )
-
+			"""
 			except ModuleNotFoundError:
 				if conf.ORQUESTRA_SHOW_NO_MODULE_EXCEPTION:
 					traceback.print_exc()
 				pass
+			"""
 			except ImportError:
 				if conf.ORQUESTRA_SHOW_NO_MODULE_EXCEPTION:
 					traceback.print_exc()
