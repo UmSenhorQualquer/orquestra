e5f703d817c7bc14015af8428039d5aa6dab1d5c
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-11-02 00:04:29 +0000

    add requirements

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
 		'Topic :: Internet :: WWW/HTTP',
 		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
 	],
+    install_requires=['django-allauth'],
 	package_data={'orquestra': [
 		'static/*.*',
 		'static/jquery-ui/*.js',
