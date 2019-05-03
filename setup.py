ad3f0767b51306c9c0335ef259d91fefe472f29a
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-05-03 13:19:29 +0100

    Update to version 1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
 	name='Orquestra',
-	version='1.1',
+	version='1.2',
 	packages=find_packages(),
 	include_package_data=True,
 	description='There for pyforms web.',
