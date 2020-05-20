907cfb69634562ebe079848174f2a5e2378411fd
UmSenhorQualquer <ricardojvr@gmail.com>
2020-05-20 18:20:58 +0100

    Fix issues

--- orquestra/views.py
+++ orquestra/views.py
@@ -84,7 +84,8 @@ def index(request, app_uid=None):
     for menu in menus: menu.submenus = sorted(menu.submenus, key=lambda x: x.order)
     #################################################################################################
 
-    if running_menu is None and len(menus)>0: running_menu = sorted(menus, key=lambda x: x.order )[0]
+    if running_menu is None and len(menus)>0 and app_uid is None:
+        running_menu = sorted(menus, key=lambda x: x.order )[0]
 
     context = {'user': request.user}
     context.update({
