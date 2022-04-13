870290bfaac4f5d2c0902c0cb7c03f84e8fac124
Ricardo Ribeiro <ricardojvr@gmail.com>
2022-04-13 19:08:50 +0100

    .

--- orquestra/static/orquestra.js
+++ orquestra/static/orquestra.js
@@ -290,7 +290,7 @@ function show_window(name, label, url, bigwindow) {
 				var window_exists = $('#'+dialog_id).length>0;
 				var extra_css = '';
 				if(bigwindow)
-					var extra_css = 'large';
+					extra_css = 'fullscreen';
 				if( !window_exists )
 					$('body').append(`<div class='ui ${extra_css} modal' id='${dialog_id}' ></div>`);
 
